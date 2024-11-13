17e90e237d63b78c53e5f44392322ecdd3db3d95
Cong Le <longchike@gmail.com>
2024-11-13 10:44:39 -0800

    Organize code and add some examples of using UIViewRepresentable and NSViewRepresentable in SwiftUI

---
+++ MyApp/SwiftUIViewAccessibilityExamples/ViewRepresentableExample.swift
@@ -0,0 +1,46 @@
+//
+//  ViewRepresentableExample.swift
+//  MyApp
+//
+//  Created by Cong Le on 11/13/24.
+//
+
+
+import SwiftUI
+
+/// Examples of using UIViewRepresentable and NSViewRepresentable in SwiftUI
+/// accessibility.
+struct ViewRepresentableExample: View {
+    var body: some View {
+        LabeledExample("Element with representable view") {
+            // You can use the SwiftUI Accessibility API to customize the accessibility of
+            // AppKit or UIKit represented elements.
+            RepresentableView()
+                .frame(width: 128, height: 48)
+                .accessibilityLabel("representable view accessibility label")
+                .accessibilityValue("representable view accessibility value")
+        }
+
+        LabeledExample("Element with representable view controller") {
+            RepresentableViewController()
+                .frame(width: 128, height: 48)
+                .accessibilityLabel("representable view controller accessibility label")
+                .accessibilityValue("representable view controller accessibility value")
+        }
+
+        LabeledExample("Combining accessibility for representable views") {
+            // Using the `accessibilityElement` modifier to combine or ignore children
+            // hides the AppKit or UIKit represented element to accessibility, and
+            // instead assistive technologies interact with the newly created
+            // accessibility element.
+            RepresentableView()
+                .frame(width: 128, height: 48)
+                .accessibilityElement(children: .combine)
+        }
+    }
+}
+
+// MARK: - Preview 
+#Preview {
+    ViewRepresentableExample()
+}
