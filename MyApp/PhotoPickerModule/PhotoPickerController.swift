6e76ed2713b6b382bdb8ccd0e63bf914c67d8d8b
Cong Le <longchike@gmail.com>
2024-11-16 22:46:23 -0800

    optimmize code

--- MyApp/PhotoPickerModule/PhotoPickerController.swift
+++ MyApp/PhotoPickerModule/PhotoPickerController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import PhotosUI
-import Foundation
 
 class PhotoPickerController: UIViewController, PHPickerViewControllerDelegate {
     
@@ -20,91 +19,65 @@ class PhotoPickerController: UIViewController, PHPickerViewControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
+    }
+    
+    private func setupUI() {
         view.addSubview(selectPhotoButton)
-        
         NSLayoutConstraint.activate([
             selectPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             selectPhotoButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
-        
         selectPhotoButton.addTarget(self, action: #selector(selectPhotoButtonTapped), for: .touchUpInside)
     }
     
-    @objc func selectPhotoButtonTapped() {
+    @objc private func selectPhotoButtonTapped() {
         checkPhotoLibraryPermission()
     }
     
-    func checkPhotoLibraryPermission() {
-        let status = PHPhotoLibrary.authorizationStatus()
-        
-        switch status {
+    private func checkPhotoLibraryPermission() {
+        switch PHPhotoLibrary.authorizationStatus() {
         case .authorized:
-            // Access already granted, proceed.
-            print("Photo library access already authorized.")
             presentPicker()
-        case .denied, .restricted:
-            // Handle denied or restricted access.
-            print("Photo library access denied or restricted.")
         case .notDetermined:
-            // Request authorization
-            //            PHPhotoLibrary.requestAuthorization { newStatus in
-            //                if newStatus == .authorized {
-            //                    DispatchQueue.main.async {
-            //                        self.presentPicker()
-            //                    }
-            //                }
-            //            }
-            PhotoPermissionsManager.requestPhotoLibraryPermission { status in
-                // Handle the authorization response as above in the request snippet
-                switch status {
-                case .authorized:
-                    // Access already granted, proceed.
-                    print("Photo library access authorized.")
-                case .denied:
-                    // Handle denied access.
-                    print("Photo library access denied.")
-                case .restricted:
-                    // Handle restricted access.
-                    print("Photo library access restricted.")
-                case .notDetermined:
-                    print("Photo library access not determined.")
-                case .limited:
-                    print("Photo library access limited.")
-                @unknown default:
-                    print("Unknown photo library access status.")
-                }
-            }
-        case .limited:
-            let alert = UIAlertController(title: "Access Denied", message: "Please enable photo library access in Settings.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-            
-            print("Photo library access limited.")
-        @unknown default:
-            print("Unknown photo library access status.")
+            requestPhotoLibraryAccess()
+        default:
+            showPermissionDeniedAlert()
         }
     }
     
-    func presentPicker() {
+    private func requestPhotoLibraryAccess() {
+        PhotoPermissionsManager.requestPhotoLibraryPermission { [weak self] status in
+            if status == .authorized {
+                self?.presentPicker()
+            } else {
+                self?.showPermissionDeniedAlert()
+            }
+        }
+    }
+    
+    private func showPermissionDeniedAlert() {
+        let alert = UIAlertController(title: "Access Denied", message: "Please enable photo library access in Settings.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func presentPicker() {
         var configuration = PHPickerConfiguration()
         configuration.filter = .images
         let picker = PHPickerViewController(configuration: configuration)
         picker.delegate = self
-        self.present(picker, animated: true, completion: nil)
+        present(picker, animated: true, completion: nil)
     }
     
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
-        picker.dismiss(animated: true, completion: nil)
-        
-        guard let provider = results.first?.itemProvider else { return }
-        
-        if provider.canLoadObject(ofClass: UIImage.self) {
+        picker.dismiss(animated: true) { [weak self] in
+            guard let provider = results.first?.itemProvider, provider.canLoadObject(ofClass: UIImage.self) else { return }
             provider.loadObject(ofClass: UIImage.self) { image, _ in
-                if image is UIImage {
+                if let image = image as? UIImage {
                     DispatchQueue.main.async {
-                        // Update UI with the image
-                        print("TODO: Update UI with the image")
+                        // TODO: Update UI with the image
+                        print("Image selected and available")
                     }
                 }
             }
@@ -113,7 +86,6 @@ class PhotoPickerController: UIViewController, PHPickerViewControllerDelegate {
 }
 
 class PhotoPermissionsManager {
-    
     static func requestPhotoLibraryPermission(completion: @escaping (PHAuthorizationStatus) -> Void) {
         PHPhotoLibrary.requestAuthorization { status in
             DispatchQueue.main.async {
