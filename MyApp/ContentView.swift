5b9eedfad3c5b139c74c06afba0975441da118be
Cong Le <longchike@gmail.com>
2024-11-19 19:18:11 -0800

    clean up and organize code - show different level of previews and entry points when rendering views

--- MyApp/ContentView.swift
+++ MyApp/ContentView.swift
@@ -7,22 +7,41 @@
 
 import SwiftUI
 
-// Step 2: Use in SwiftUI view
-struct ContentView: View {
+// Use in SwiftUI view
+struct ContentView: View { // The app will load view from this entry point
     var body: some View {
-        UIKitViewControllerWrapper()
+        UIKitViewControllerWrapper_WKWebViewController()
             .edgesIgnoringSafeArea(.all) /// Ignore safe area to extend the background color to the entire screen
     }
 }
 
+// MARK: - Using PreviewProvider to preview views - connecting UIKit views
+/// The name of the view when loaded on the canvas screen is determined
+/// by the name of the struct that conforms to the `PreviewProvider` protocol,
+/// excluding any reserved system keywords and
+/// removing the `_Previews` suffix from the struct's name.
+
 // Before iOS 17, use this syntax for preview UIKit view controller
-struct UIKitViewControllerWrapper_Previews: PreviewProvider {
+struct WKWebView_Previews: PreviewProvider { // The previews on canvas will load from this entry point
     static var previews: some View {
-        UIKitViewControllerWrapper()
+        UIKitViewControllerWrapper_WKWebViewController()
     }
 }
 
-// After iOS 17, we can use this syntax for preview:
+struct SafariView_Previews: PreviewProvider {
+    static var previews: some View {
+        UIKitViewControllerWrapper_SFViewController()
+    }
+}
+
+struct RichTextView_Previews: PreviewProvider {
+    static var previews: some View {
+        UIKitViewControllerWrapper_RichTextViewController()
+    }
+}
+
+// MARK: - Using macros to preview views - connecting SwiftUI views
+// After iOS 17, we can use this syntax for preview
 #Preview {
     ContentView()
 }
--- MyApp/UIKitViewControllerWrapper.swift
+++ MyApp/UIKitViewControllerWrapper.swift
@@ -7,15 +7,14 @@
 
 import SwiftUI
 import UIKit
-@preconcurrency import WebKit
 
-// Step 1a: UIViewControllerRepresentable implementation
-struct UIKitViewControllerWrapper: UIViewControllerRepresentable {
+// UIViewControllerRepresentable implementation for WKWebViewController
+struct UIKitViewControllerWrapper_WKWebViewController: UIViewControllerRepresentable {
     typealias UIViewControllerType = WKWebViewController
     
-    // Step 1b: Required methods implementation
+    // Required methods implementation
     func makeUIViewController(context: Context) -> WKWebViewController {
-        // Step 1c: Instantiate and return the UIKit view controller
+        // Instantiate and return the UIKit view controller
         return WKWebViewController()
     }
     
@@ -24,147 +23,32 @@ struct UIKitViewControllerWrapper: UIViewControllerRepresentable {
     }
 }
 
-//MARK: - MyUIKitViewController
-class WKWebViewController: UIViewController {
-    lazy var webView: WKWebView = {
-        
-        // Create web view configuration
-        let webViewConfiguration = WKWebViewConfiguration()
-        
-        if #available(iOS 14.0, *) {
-            // Set default webpage preferences
-            let webpagePreferences = WKWebpagePreferences()
-            webpagePreferences.allowsContentJavaScript = true // Enable JavaScript
-            webViewConfiguration.defaultWebpagePreferences = webpagePreferences
-        } else {
-            // Fallback on iOS 13 and ealier
-            webViewConfiguration.preferences.javaScriptEnabled = true
-        }
-        
-        
-        // Configure user content controller
-        let userContentController = WKUserContentController()
-        
-        // Add script message handler
-        userContentController.add(self, name: "callbackHandler")
-        webViewConfiguration.userContentController = userContentController
-        
-        
-        // Initialize webView with configuration
-        let webView = WKWebView(frame: .zero, configuration: webViewConfiguration)
-        webView.navigationDelegate = self
-        webView.uiDelegate = self
-        
-        return webView
-    }()
+// UIViewControllerRepresentable implementation for SFViewController
+struct UIKitViewControllerWrapper_SFViewController: UIViewControllerRepresentable {
+    typealias UIViewControllerType = SFViewController
     
-    override func loadView() {
-        self.view = webView
+    // Required methods implementation
+    func makeUIViewController(context: Context) -> SFViewController {
+        // Instantiate and return the UIKit view controller
+        return SFViewController()
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Load web content
-        // loadHTMLWebContent()
-        loadWebContent()
-    }
-    
-    func loadWebContent() {
-        // Example URL
-        if let url = URL(string: "https://www.google.com") {
-            let request = URLRequest(url: url)
-            webView.load(request)
-        }
-    }
-    
-    func loadHTMLWebContent() {
-        let htmlString = """
-        <html>
-        <head>
-            <title>WKWebView Test</title>
-            <script type="text/javascript">
-                function sendMessage() {
-                    window.webkit.messageHandlers.callbackHandler.postMessage('Button clicked!');
-                }
-            </script>
-        </head>
-        <body>
-            <h1>WKWebView Integration</h1>
-            <button type="button" onclick="sendMessage()">Click Me!</button>
-        </body>
-        </html>
-        """
-        webView.loadHTMLString(htmlString, baseURL: nil)
+    func updateUIViewController(_ uiViewController: SFViewController, context: Context) {
+        // Update the view controller if needed
     }
 }
 
-// MARK: - WKNavigationDelegate Methods
-extension WKWebViewController: WKNavigationDelegate {
+// UIViewControllerRepresentable implementation for RichTextViewController
+struct UIKitViewControllerWrapper_RichTextViewController: UIViewControllerRepresentable {
+    typealias UIViewControllerType = RichTextViewController
     
-    // MARK: - WKNavigationDelegate Methods
-    
-    func webView(_ webView: WKWebView,
-                 didStartProvisionalNavigation navigation: WKNavigation!
-    ){
-        print("Started to load")
+    // Required methods implementation
+    func makeUIViewController(context: Context) -> RichTextViewController {
+        // Instantiate and return the UIKit view controller
+        return RichTextViewController()
     }
     
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print("Finished loading")
-    }
-    
-    // Handle navigation actions (e.g., decide policy for navigation)
-    func webView(_ webView: WKWebView,
-                 decidePolicyFor navigationAction: WKNavigationAction,
-                 decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
-    ){
-        // Allow navigation
-        decisionHandler(.allow)
-    }
-    
-    // Optional: Handle per-navigation preferences
-    func webView(_ webView: WKWebView,
-                 decidePolicyFor navigationAction: WKNavigationAction,
-                 preferences: WKWebpagePreferences,
-                 decisionHandler: @escaping (WKNavigationActionPolicy, WKWebpagePreferences) -> Void
-    ){
-        // Enable JavaScript for this navigation
-        preferences.allowsContentJavaScript = true
-        decisionHandler(.allow, preferences)
-    }
-}
-
-// MARK: - WKUIDelegate Methods
-extension WKWebViewController: WKUIDelegate {
-    // Handle JavaScript alerts
-    func webView(_ webView: WKWebView,
-                 runJavaScriptAlertPanelWithMessage message: String,
-                 initiatedByFrame frame: WKFrameInfo,
-                 completionHandler: @escaping () -> Void
-    ){
-        let alertController = UIAlertController(
-            title: "Alert from Webpage",
-            message: message,
-            preferredStyle: .alert
-        )
-        
-        alertController.addAction(
-            UIAlertAction(title: "OK", style: .default) { _ in
-                completionHandler()
-            })
-        present(alertController, animated: true, completion: nil)
-    }
-}
-
-// MARK: - WKScriptMessageHandler Methods
-extension WKWebViewController: WKScriptMessageHandler {
-    // Handle messages from JavaScript
-    func userContentController(_ userContentController: WKUserContentController,
-                               didReceive message: WKScriptMessage) {
-        if message.name == "callbackHandler", let body = message.body as? String {
-            print("JavaScript is sending a message \(body)")
-            // Process message as needed
-        }
+    func updateUIViewController(_ uiViewController: RichTextViewController, context: Context) {
+        // Update the view controller if needed
     }
 }
