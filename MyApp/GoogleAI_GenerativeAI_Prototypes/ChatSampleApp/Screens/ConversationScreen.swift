b10c4d037e8fd661061ace85b07c900d63a8c5dc
Cong Le <longchike@gmail.com>
2024-11-23 04:49:38 -0800

    add ChatSampleApp

---
+++ MyApp/GoogleAI_GenerativeAI_Prototypes/ChatSampleApp/Screens/ConversationScreen.swift
@@ -0,0 +1,129 @@
+//
+//  ConversationScreen.swift
+//  MyApp
+//
+//  Created by Cong Le on 11/23/24.
+//
+
+
+
+/// I will add this static GenerativeAIUIComponents framework later,
+/// I'm using direct implementation from the original source code for now
+// import GenerativeAIUIComponents
+
+
+import GoogleGenerativeAI
+import SwiftUI
+
+struct ConversationScreen: View {
+  @EnvironmentObject
+  var viewModel: ConversationViewModel
+
+  @State
+  private var userPrompt = ""
+
+  enum FocusedField: Hashable {
+    case message
+  }
+
+  @FocusState
+  var focusedField: FocusedField?
+
+  var body: some View {
+    VStack {
+      ScrollViewReader { scrollViewProxy in
+        List {
+          ForEach(viewModel.messages) { message in
+            MessageView(message: message)
+          }
+          if let error = viewModel.error {
+            ErrorView(error: error)
+              .tag("errorView")
+          }
+        }
+        .listStyle(.plain)
+        .onChange(of: viewModel.messages, perform: { newValue in
+          if viewModel.hasError {
+            // wait for a short moment to make sure we can actually scroll to the bottom
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+              withAnimation {
+                scrollViewProxy.scrollTo("errorView", anchor: .bottom)
+              }
+              focusedField = .message
+            }
+          } else {
+            guard let lastMessage = viewModel.messages.last else { return }
+
+            // wait for a short moment to make sure we can actually scroll to the bottom
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+              withAnimation {
+                scrollViewProxy.scrollTo(lastMessage.id, anchor: .bottom)
+              }
+              focusedField = .message
+            }
+          }
+        })
+      }
+      InputField("Message...", text: $userPrompt) {
+        Image(systemName: viewModel.busy ? "stop.circle.fill" : "arrow.up.circle.fill")
+          .font(.title)
+      }
+      .focused($focusedField, equals: .message)
+      .onSubmit { sendOrStop() }
+    }
+    .toolbar {
+      ToolbarItem(placement: .primaryAction) {
+        Button(action: newChat) {
+          Image(systemName: "square.and.pencil")
+        }
+      }
+    }
+    .navigationTitle("Chat sample")
+    .onAppear {
+      focusedField = .message
+    }
+  }
+
+  private func sendMessage() {
+    Task {
+      let prompt = userPrompt
+      userPrompt = ""
+      await viewModel.sendMessage(prompt, streaming: true)
+    }
+  }
+
+  private func sendOrStop() {
+    focusedField = nil
+
+    if viewModel.busy {
+      viewModel.stop()
+    } else {
+      sendMessage()
+    }
+  }
+
+  private func newChat() {
+    viewModel.startNewChat()
+  }
+}
+
+// MARK: - Previews
+struct ConversationScreen_Previews: PreviewProvider {
+  struct ContainerView: View {
+    @StateObject var viewModel = ConversationViewModel()
+
+    var body: some View {
+      ConversationScreen()
+        .environmentObject(viewModel)
+        .onAppear {
+          viewModel.messages = ChatMessage.samples
+        }
+    }
+  }
+
+  static var previews: some View {
+    NavigationStack {
+      ConversationScreen()
+    }
+  }
+}
--- MyApp/MyApp.swift
+++ MyApp/MyApp.swift
@@ -4,34 +4,34 @@
 //
 //  Created by Cong Le on 8/19/24.
 //
-
-import SwiftUI
-
-@main
-struct MyApp: App {
-    // Monitor the app's scene phase (active, inactive, background)
-    @Environment(\.scenePhase) private var scenePhase
-
-    init() {
-        // Equivalent to application(_:didFinishLaunchingWithOptions:)
-        print("App initialized (didFinishLaunchingWithOptions)")
-    }
-
-    var body: some Scene {
-        WindowGroup {
-            RootContentView()
-        }// Respond to changes in the scene phase
-        .onChange(of: scenePhase) { oldPhase, newPhase in
-            switch newPhase {
-            case .active:
-                print("Scene became active")
-            case .inactive:
-                print("Scene became inactive")
-            case .background:
-                print("Scene entered background")
-            @unknown default:
-                print("Unknown scene phase")
-            }
-        }
-    }
-}
+//
+//import SwiftUI
+//
+//@main
+//struct MyApp: App {
+//    // Monitor the app's scene phase (active, inactive, background)
+//    @Environment(\.scenePhase) private var scenePhase
+//
+//    init() {
+//        // Equivalent to application(_:didFinishLaunchingWithOptions:)
+//        print("App initialized (didFinishLaunchingWithOptions)")
+//    }
+//
+//    var body: some Scene {
+//        WindowGroup {
+//            RootContentView()
+//        }// Respond to changes in the scene phase
+//        .onChange(of: scenePhase) { oldPhase, newPhase in
+//            switch newPhase {
+//            case .active:
+//                print("Scene became active")
+//            case .inactive:
+//                print("Scene became inactive")
+//            case .background:
+//                print("Scene entered background")
+//            @unknown default:
+//                print("Unknown scene phase")
+//            }
+//        }
+//    }
+//}
