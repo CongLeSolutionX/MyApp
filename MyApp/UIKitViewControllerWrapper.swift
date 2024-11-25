4f2f280f2d83f90983668ee336c0e1ff3133ba64
Cong Le <longchike@gmail.com>
2024-11-24 17:22:36 -0800

    organize code

--- MyApp/UIKitViewControllerWrapper.swift
+++ MyApp/UIKitViewControllerWrapper.swift
@@ -10,24 +10,25 @@ import UIKit
 
 // Step 1a: UIViewControllerRepresentable implementation
 struct UIKitViewControllerWrapper: UIViewControllerRepresentable {
-    typealias UIViewControllerType = AccessibleImagesViewController
+    typealias UIViewControllerType = MyUIKitViewController
     
     // Step 1b: Required methods implementation
-    func makeUIViewController(context: Context) -> AccessibleImagesViewController {
+    func makeUIViewController(context: Context) -> MyUIKitViewController {
         // Step 1c: Instantiate and return the UIKit view controller
-        return AccessibleImagesViewController()
+        return MyUIKitViewController()
     }
     
-    func updateUIViewController(_ uiViewController: AccessibleImagesViewController, context: Context) {
+    func updateUIViewController(_ uiViewController: MyUIKitViewController, context: Context) {
         // Update the view controller if needed
     }
 }
 
 // Example UIKit view controller
-//class AccessibleImagesViewController: UIViewController {
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        view.backgroundColor = .systemBlue
-//        // Additional setup
-//    }
-//}
+class MyUIKitViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        // Additional setup
+    }
+}
+
