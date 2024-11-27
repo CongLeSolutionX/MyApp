1a79c7fcf5680e96bb7c5290ba6a07e66b8be570
Cong Le <longchike@gmail.com>
2024-11-26 16:07:40 -0800

    add Position-based Hue effect

---
+++ MyApp/VisualEffectsUsingMetal/VisualEffects.swift
@@ -0,0 +1,65 @@
+//
+//  VisualEffects.swift
+//  MyApp
+//
+//  Created by Cong Le on 11/26/24.
+//
+
+/*
+ Source: https://developer.apple.com/documentation/swiftui/creating-visual-effects-with-swiftui
+ 
+Abstract:
+Examples for applying visual effects to a view based on its position inside a
+ `ScrollView`.
+*/
+
+import SwiftUI
+
+// MARK: Position-based Hue
+#Preview("Position-based Hue") {
+    ScrollView(.vertical) {
+        VStack {
+            ForEach(0 ..< 20) { _ in
+                RoundedRectangle(cornerRadius: 24)
+                    .fill(.purple)
+                    .frame(height: 100)
+                    .visualEffect { content, proxy in
+                        content
+                            .hueRotation(.degrees(proxy.frame(in: .global).origin.y / 10))
+                    }
+            }
+        }
+        .padding()
+    }
+}
+
+// MARK: Position-based Hue & Scale
+#Preview("Position-based Hue & Scale") {
+    ScrollView(.vertical) {
+        VStack {
+            ForEach(0 ..< 20) { _ in
+                RoundedRectangle(cornerRadius: 24)
+                    .fill(.purple)
+                    .frame(height: 100)
+                    .visualEffect { content, proxy in
+                        let frame = proxy.frame(in: .scrollView(axis: .vertical))
+                        let parentBounds = proxy
+                            .bounds(of: .scrollView(axis: .vertical)) ??
+                            .infinite
+
+                        // The distance this view extends past the bottom edge
+                        // of the scroll view.
+                        let distance = min(0, frame.minY)
+
+                        return content
+                            .hueRotation(.degrees(frame.origin.y / 10))
+                            .scaleEffect(1 + distance / 700)
+                            .offset(y: -distance / 1.25)
+                            .brightness(-distance / 400)
+                            .blur(radius: -distance / 50)
+                    }
+            }
+        }
+        .padding()
+    }
+}
