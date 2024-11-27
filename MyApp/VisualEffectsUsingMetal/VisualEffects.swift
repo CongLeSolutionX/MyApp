94546e30b46b1b46c4f451dc79ae028e0c5b9f0c
Cong Le <longchike@gmail.com>
2024-11-26 16:37:00 -0800

    add more visual  effects

--- MyApp/VisualEffectsUsingMetal/VisualEffects.swift
+++ MyApp/VisualEffectsUsingMetal/VisualEffects.swift
@@ -15,6 +15,38 @@ Examples for applying visual effects to a view based on its position inside a
 
 import SwiftUI
 
+// MARK: Faster Hue Shift
+#Preview("Faster Hue Shift") {
+    ScrollView(.vertical) {
+        VStack {
+            ForEach(0 ..< 20) { _ in
+                RoundedRectangle(cornerRadius: 24)
+                    .fill(.purple)
+                    .frame(height: 100)
+                    .visualEffect { content, proxy in
+                        let frame = proxy.frame(in: .scrollView(axis: .vertical))
+                        let parentBounds = proxy
+                            .bounds(of: .scrollView(axis: .vertical)) ??
+                            .infinite
+
+                        // The distance this view extends past the bottom edge
+                        // of the scroll view.
+                        let distance = min(0, frame.minY)
+
+                        return content
+                            .hueRotation(.degrees(frame.origin.y / 5)) ///  Increase the divisor to make the hue change more rapidly.
+                            .scaleEffect(1 + distance / 700)
+                            .offset(y: -distance / 1.25)
+                            .brightness(-distance / 400)
+                            .blur(radius: -distance / 50)
+                    }
+            }
+        }
+        .padding()
+    }
+}
+
+
 // MARK: Position-based Hue
 #Preview("Position-based Hue") {
     ScrollView(.vertical) {
---
+++ MyApp/VisualEffectsUsingMetal/VisualEffects2.swift
@@ -0,0 +1,40 @@
+//
+//  VisualEffectss.swift
+//  MyApp
+//
+//  Created by Cong Le on 11/26/24.
+//
+
+import SwiftUI
+
+
+// MARK: Sine Wave Hue Shift
+#Preview("Sine Wave Hue Shift") {
+    ScrollView(.vertical) {
+        VStack {
+            ForEach(0 ..< 20) { _ in
+                RoundedRectangle(cornerRadius: 24)
+                    .fill(.purple)
+                    .frame(height: 100)
+                    .visualEffect { content, proxy in
+                        let frame = proxy.frame(in: .scrollView(axis: .vertical))
+                        let parentBounds = proxy
+                            .bounds(of: .scrollView(axis: .vertical)) ??
+                            .infinite
+
+                        // The distance this view extends past the bottom edge
+                        // of the scroll view.
+                        let distance = min(0, frame.minY)
+
+                        return content
+                            .hueRotation(.degrees(frame.origin.y / 10 + Double(index) * 36))
+                            .scaleEffect(1 + distance / 700)
+                            .offset(y: -distance / 1.25)
+                            .brightness(-distance / 400)
+                            .blur(radius: -distance / 50)
+                    }
+            }
+        }
+        .padding()
+    }
+}
