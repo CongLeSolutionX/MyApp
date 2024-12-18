1ac87fde15eb38e6229f7273863977ce782ac49c
Cong Le <longchike@gmail.com>
2024-12-17 17:55:47 -0800

    documentation

--- MyApp/Utils/CoreGraphics+Extensions.swift
+++ MyApp/Utils/CoreGraphics+Extensions.swift
@@ -4,6 +4,8 @@
 //
 //  Created by Cong Le on 12/17/24.
 //
+// Source: https://github.com/dehesa/sample-metal/tree/main/Metal%20By%20Example/Clear%20Screen
+
 
 import CoreGraphics
 
--- MyApp/Utils/Foundation+Extensions.swift
+++ MyApp/Utils/Foundation+Extensions.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Cong Le on 12/17/24.
 //
+// Source: https://github.com/dehesa/sample-metal/tree/main/Metal%20By%20Example/Clear%20Screen
 
 import Foundation
 
