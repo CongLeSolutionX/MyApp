abcfae8d5d79670c3daca99b96ae69933b892035
Cong Le <longchike@gmail.com>
2024-12-18 22:02:05 -0800

    add shader vertex for 2D view

---
+++ MyApp/Metal_Basic-Views/ShaderVertex.swift
@@ -0,0 +1,13 @@
+//
+//  ShaderVertex.swift
+//  MyApp
+//
+//  Created by Cong Le on 12/18/24.
+//
+
+/// Note: Set up to draw 2D view
+/// The vertices being fed to the GPU.
+struct ShaderVertex {
+  var position: SIMD4<Float>
+  var color: SIMD4<Float>
+}
