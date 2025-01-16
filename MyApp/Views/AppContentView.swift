eb7410d0472c0daefae7db5eed4eaf97fa62e8ff
CongLeSolutionX <cle155@csu.fullerton.edu>
2025-01-16 00:09:18 -0800

    Conditional Presentation of NavigationStack

--- MyApp/Views/AppContentView.swift
+++ MyApp/Views/AppContentView.swift
@@ -9,42 +9,53 @@ import SwiftUI
 
 struct AppContentView: View {
     @StateObject var appCoordinator = AppCoordinator()
-
+    @State private var showNavigation = false
+    
     var body: some View {
-        NavigationStack(path: $appCoordinator.navigationPath) {
-            // Initial view when the NavigationStack is created
-            HomeView()
-                .environmentObject(appCoordinator)
-                .navigationDestination(for: AppCoordinator.AppPage.self) { page in
-                    switch page {
-                    case .home:
-                        // Re-provide HomeView, though it's already the root
-                        HomeView()
-                            .environmentObject(appCoordinator)
-                    case .settings:
-                        if let settingsCoordinator = appCoordinator.settingsCoordinator {
-                            SettingsView()
-                                .environmentObject(settingsCoordinator)
+        Group {
+            if showNavigation {
+                NavigationStack(path: $appCoordinator.navigationPath) {
+                    HomeView()
+                        .environmentObject(appCoordinator)
+                        .navigationDestination(for: AppCoordinator.AppPage.self) { page in
+                            switch page {
+                            case .home:
+                                HomeView()
+                                    .environmentObject(appCoordinator)
+                            case .settings:
+                                if let settingsCoordinator = appCoordinator.settingsCoordinator {
+                                    SettingsView()
+                                        .environmentObject(settingsCoordinator)
+                                }
+                            case .profile(let userID):
+                                ProfileView(userID: userID)
+                            case .productDetail(let product):
+                                ProductDetailView(product: product)
+                            }
                         }
-                    case .profile(let userID):
-                        ProfileView(userID: userID)
-                    case .productDetail(let product):
-                        ProductDetailView(product: product)
-                    }
-                }
-                .navigationDestination(for: SettingsCoordinator.SettingsPage.self) { page in
-                    switch page {
-                    case .main:
-                        if let settingsCoordinator = appCoordinator.settingsCoordinator {
-                            SettingsView()
-                                .environmentObject(settingsCoordinator)
+                        .navigationDestination(for: SettingsCoordinator.SettingsPage.self) { page in
+                            switch page {
+                            case .main:
+                                if let settingsCoordinator = appCoordinator.settingsCoordinator {
+                                    SettingsView()
+                                        .environmentObject(settingsCoordinator)
+                                }
+                            case .privacy:
+                                PrivacySettingsView()
+                            case .notifications:
+                                NotificationSettingsView()
+                            }
                         }
-                    case .privacy:
-                        PrivacySettingsView()
-                    case .notifications:
-                        NotificationSettingsView()
-                    }
                 }
+            } else {
+                HomeView()
+                    .environmentObject(appCoordinator)
+                    .onAppear {
+                        DispatchQueue.main.async {
+                            showNavigation = true
+                        }
+                    }
+            }
         }
         .environmentObject(appCoordinator)
         .onOpenURL { url in
