bd627fb05cab185d26455a7a7c5ee2bfb53dba06
CongLeSolutionX <cle155@csu.fullerton.edu>
2025-01-29 11:24:44 -0800

    use UIApplicationMain as the entry point for the app

--- MyApp/MyApp.swift
+++ MyApp/MyApp.swift
@@ -4,35 +4,37 @@
 //
 //  Created by Cong Le on 8/19/24.
 //
-
-import SwiftUI
-
-@main
-struct MyApp: App {
-    // Monitor the app's scene phase (active, inactive, background)
-    @Environment(\.scenePhase) private var scenePhase
-
-    init() {
-        // Equivalent to application(_:didFinishLaunchingWithOptions:)
-        print("App initialized (didFinishLaunchingWithOptions)")
-    }
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }// Respond to changes in the scene phase
-        // Respond to changes in the scene phase
-        .onChange(of: scenePhase) { oldPhase, newPhase in
-            switch newPhase {
-            case .active:
-                print("Scene became active")
-            case .inactive:
-                print("Scene became inactive")
-            case .background:
-                print("Scene entered background")
-            @unknown default:
-                print("Unknown scene phase")
-            }
-        }
-    }
-}
+//
+//import SwiftUI
+//
+//@main
+//struct MyApp: App {
+//    // Monitor the app's scene phase (active, inactive, background)
+//    @Environment(\.scenePhase) private var scenePhase
+//
+//    init() {
+//        // Equivalent to application(_:didFinishLaunchingWithOptions:)
+//        print("App initialized (didFinishLaunchingWithOptions)")
+//    }
+//
+//    var body: some Scene {
+//        
+//        WindowGroup {
+//            UIKitViewControllerWrapper()
+////            ContentView()
+//        }// Respond to changes in the scene phase
+//        // Respond to changes in the scene phase
+//        .onChange(of: scenePhase) { oldPhase, newPhase in
+//            switch newPhase {
+//            case .active:
+//                print("Scene became active")
+//            case .inactive:
+//                print("Scene became inactive")
+//            case .background:
+//                print("Scene entered background")
+//            @unknown default:
+//                print("Unknown scene phase")
+//            }
+//        }
+//    }
+//}
