dbb04e8cbabe7c1530b015ba658fc9802a83cea4
CongLeSolutionX <cle155@csu.fullerton.edu>
2025-02-16 10:28:07 -0600

    update file names and data

---
+++ MyApp/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  MyApp
+//
+//  Created by Cong Le on 8/19/24.
+//
+
+import SwiftUI
+
+// Step 2: Use in SwiftUI view
+struct ContentViewOrignail: View {
+    var body: some View {
+        UIKitViewControllerWrapper()
+            .edgesIgnoringSafeArea(.all) /// Ignore safe area to extend the background color to the entire screen
+    }
+}
+
+// Before iOS 17, use this syntax for preview UIKit view controller
+struct UIKitViewControllerWrapper_Previews: PreviewProvider {
+    static var previews: some View {
+        UIKitViewControllerWrapper()
+    }
+}
+
+// After iOS 17, we can use this syntax for preview:
+#Preview {
+    RSSContentView()
+}
