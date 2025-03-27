927dcebefa001b5e6b18e4b4efd4b31dc861964f
Cong Le <cle155@csu.fullerton.edu>
2025-03-27 15:16:51 -0700

    add Inventory view

--- MyApp/MyApp.swift
+++ MyApp/MyApp.swift
@@ -12,7 +12,7 @@ import SwiftUI
 struct MyApp: App {
     var body: some Scene {
         WindowGroup {
-            BarCodeScanningItemManagementHomeView()
+            ContentView()
         }
     }
 }
