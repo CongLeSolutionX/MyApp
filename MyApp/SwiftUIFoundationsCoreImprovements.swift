04d1fcb990823e85fb4c089c8d973ffc47a9aaf3
Cong Le <cle155@csu.fullerton.edu>
2025-04-01 18:39:30 -0700

    update code

--- MyApp/SwiftUIFoundationsCoreImprovements.swift
+++ MyApp/SwiftUIFoundationsCoreImprovements.swift
@@ -5,31 +5,6 @@
 //  Created by Cong Le on 4/1/25.
 //
 import SwiftUI
-
-// --- Color Mixing ---
-
-struct ColorMixingExample: View {
-    var body: some View {
-        VStack {
-            Text("Mixing Red and Purple")
-                .font(.headline)
-            HStack {
-                Color.red
-                Color.red.mix(with: .purple, by: 0.2)
-                Color.red.mix(with: .purple, by: 0.5) // Equal mix
-                Color.red.mix(with: .purple, by: 0.8)
-                Color.purple
-            }
-            .frame(height: 100)
-        }
-        .padding()
-    }
-}
-
-#Preview("Color Mixing") {
-    ColorMixingExample()
-}
-
 // --- Shader Precompilation ---
 
 // Placeholder - Assume ShaderLibrary.myCoolEffect() returns a Shader
