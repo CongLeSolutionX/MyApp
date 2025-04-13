01fd3c7dca00d999554419290589a2e54615b0c6
Cong Le <cle155@csu.fullerton.edu>
2025-04-13 11:22:14 -0700

    1st try - lovely work

---
+++ MyApp/ImageSliderContentView.swift
@@ -0,0 +1,178 @@
+//
+//  ImageSliderContentView.swift
+//  MyApp
+//
+//  Created by Cong Le on 4/13/25.
+//
+
+
+import SwiftUI
+
+/// Image Model and Sample Data
+struct ImageModel: Identifiable {
+    var id: String = UUID().uuidString
+    var altText: String
+    /// Going to use Live URL Image via AsyncImage for this effect.
+    var link: String
+}
+
+let sampleImages: [ImageModel] = [
+    .init(
+        altText: "Mo Eid",
+        link: "https://images.pexels.com/photos/9002742/pexels-photo-9002742.jpeg?cs=srgb&dl=pexels-mo-eid-1268975-9002742.jpg&fm=jpg&w=640&h=405"
+    ),
+    .init(
+        altText: "Codioful",
+        link: "https://images.pexels.com/photos/7135121/pexels-photo-7135121.jpeg?cs=srgb&dl=pexels-codioful-7135121.jpg&fm=jpg&w=640&h=427"
+    ),
+    .init(
+        altText: "Fanny Hagan",
+        link: "https://images.pexels.com/photos/19896879/pexels-photo-19896879.jpeg?cs=srgb&dl=pexels-fanny-hagan-842972996-19896879.jpg&fm=jpg&w=640&h=550"
+    ),
+    .init(
+        altText: "Han-Chieh Lee",
+        link: "https://images.pexels.com/photos/22944463/pexels-photo-22944463.jpeg?cs=srgb&dl=pexels-han-chieh-lee-1234591373-22944463.jpg&fm=jpg&w=640&h=427"
+    ),
+    .init(
+        altText: "Cottonbro",
+        link: "https://images.pexels.com/photos/9669094/pexels-photo-9669094.jpeg?cs=srgb&dl=pexels-cottonbro-9669094.jpg&fm=jpg&w=640&h=960"
+    ),
+    .init(
+        altText: "Gülşah Aydoğan",
+        link: "https://images.pexels.com/photos/18873058/pexels-photo-18873058.jpeg?cs=srgb&dl=pexels-gulsahaydgn-18873058.jpg&fm=jpg&w=640&h=450"
+    )
+]
+
+struct ImageSliderContentView: View {
+    @State private var activeID: String?
+    var body: some View {
+        /// Navigation Stack is must as this uses the Zoom Transition API
+        NavigationStack {
+            VStack {
+                HStack(alignment: .top, spacing: 12) {
+                    Circle()
+                        .fill(.fill)
+                        .frame(width: 45, height: 45)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Rectangle()
+                            .fill(.fill)
+                            .frame(width: 50, height: 5)
+                        
+                        VStack(spacing: 5) {
+                            ForEach(1...4, id: \.self) { index in
+                                Rectangle()
+                                    .fill(.fill)
+                                    .frame(height: 5)
+                                    .padding(.trailing, index == 4 ? 50 : 0)
+                            }
+                        }
+                        .padding(.top, 10)
+                        .padding(.bottom, 20)
+                        
+                        let config = ImageViewerConfig(height: 100, cornerRadius: 10, spacing: 5)
+                        
+                        ImageViewer(config: config) {
+                            ForEach(sampleImages) { image in
+                                /// Animations will work even when image is loading
+                                AsyncImage(url: URL(string: image.link)) { image in
+                                    image
+                                        .resizable()
+                                        /// Fit/Fill resize will be done inside the image viewer
+                                } placeholder: {
+                                    Rectangle()
+                                        .fill(.gray.opacity(0.4))
+                                        .overlay {
+                                            ProgressView()
+                                                .tint(.blue)
+                                                .scaleEffect(0.7)
+                                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                        }
+                                }
+                                /// Updates callback only works when a view contains containerValue
+                                .containerValue(\.activeViewID, image.id)
+                            }
+                        } overlay: {
+                            OverlayView(activeID: activeID)
+                        } updates: { isPresented, activeID in
+                            self.activeID = activeID?.base as? String
+                        }
+                        
+                        HStack {
+                            Image(systemName: "message")
+                            Spacer()
+                            Image(systemName: "arrow.trianglehead.bottomleft.capsulepath.clockwise")
+                            Spacer()
+                            Image(systemName: "suit.heart")
+                            Spacer()
+                            Image(systemName: "square.and.arrow.up")
+                        }
+                        .foregroundStyle(.primary.secondary)
+                        .padding(.top, 10)
+                    }
+                    .padding(.top, 10)
+                }
+                .padding(15)
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    Text("Usage:")
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                    
+                    Text(
+                        """
+                        ImageViewer {
+                            AsyncImages/Images...
+                        }
+                        """
+                    )
+                    .monospaced()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .padding(15)
+                .background(.gray.opacity(0.15), in: .rect(cornerRadius: 15))
+                
+                Spacer(minLength: 0)
+            }
+            .padding(15)
+            .navigationTitle("Image Viewer")
+            .navigationBarTitleDisplayMode(.large)
+        }
+    }
+}
+
+/// Overlay View
+struct OverlayView: View {
+    var activeID: String?
+    @Environment(\.dismiss) private var dismiss
+    var body: some View {
+        VStack {
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "chevron.left")
+                    .font(.title3)
+                    .foregroundStyle(.white.secondary)
+                    .padding(10)
+                    .contentShape(.rect)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .overlay {
+                if let imageItem = sampleImages.first(where: { $0.id == activeID }) {
+                    Text(imageItem.altText)
+                        .font(.callout)
+                        .foregroundStyle(.white)
+                }
+            }
+            
+            Spacer(minLength: 0)
+        }
+        .padding(15)
+    }
+}
+
+
+// MARK: - Preview
+#Preview {
+    ImageSliderContentView()
+}
---
+++ MyApp/ImageViewer.swift
@@ -0,0 +1,127 @@
+//
+//  ImageViewer.swift
+//  MyApp
+//
+//  Created by Cong Le on 4/13/25.
+//
+
+
+import SwiftUI
+
+struct ImageViewer<Content: View, Overlay: View>: View {
+    /// Config
+    var config = ImageViewerConfig()
+    @ViewBuilder var content: Content
+    @ViewBuilder var overlay: Overlay
+    /// Giving updates to the main view
+    var updates: (Bool, AnyHashable?) -> () = { _, _ in }
+    /// View Properties
+    @State private var isPresented: Bool = false
+    @State private var activeTabID: Subview.ID?
+    @State private var transitionSource: Int = 0
+    @Namespace private var animation
+    var body: some View {
+        Group(subviews: content) { collection in
+            /// Going to use new iOS 18 API to retrieve the Subview collection from the given view content
+            LazyVGrid(columns: Array(repeating: GridItem(spacing: config.spacing), count: collection.count > 3 ? 2 : collection.count), spacing: config.spacing) {
+                /// Only displaying the first four images, and the remaining ones showing a count (like +4) at the fourth image, similar to the X (Twitter) app
+                let remainingCount = max(collection.count - 4, 0)
+                ForEach(collection.prefix(4)) { item in
+                    let index = collection.index(item.id)
+                    GeometryReader {
+                        let size = $0.size
+                        
+                        item
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: size.width, height: size.height)
+                            .clipShape(.rect(cornerRadius: config.cornerRadius))
+                        
+                        if collection.prefix(4).last?.id == item.id, remainingCount > 0 {
+                            RoundedRectangle(cornerRadius: config.cornerRadius)
+                                .fill(.black.opacity(0.35))
+                                .overlay {
+                                    Text("+\(remainingCount)")
+                                        .font(.largeTitle)
+                                        .fontWeight(.semibold)
+                                        .foregroundStyle(.white)
+                                }
+                        }
+                    }
+                    .frame(height: config.height)
+                    .frame(maxWidth: .infinity)
+                    .contentShape(.rect)
+                    .onTapGesture {
+                        /// For opening the selected image in the detail tab view
+                        activeTabID = item.id
+                        /// For opening navigation detail view
+                        isPresented = true
+                        /// For Zoom Transition
+                        transitionSource = index
+                    }
+                    .matchedTransitionSource(id: index, in: animation) { config in
+                        config
+                            .clipShape(.rect(cornerRadius: self.config.cornerRadius))
+                    }
+                }
+            }
+            .navigationDestination(isPresented: $isPresented) {
+                TabView(selection: $activeTabID) {
+                    ForEach(collection) { item in
+                        item
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .tag(item.id)
+                    }
+                }
+                .tabViewStyle(.page)
+                .background {
+                    Rectangle()
+                        .fill(.black)
+                        .ignoresSafeArea()
+                }
+                .overlay {
+                    overlay
+                }
+                .navigationTransition(.zoom(sourceID: transitionSource, in: animation))
+                /// Hiding Tool Bar
+                .toolbarVisibility(.hidden, for: .navigationBar)
+            }
+            /// Updating transitionSource when tab item get's changed
+            .onChange(of: activeTabID) { oldValue, newValue in
+                /// Consider this example: when the tab view at detail view is at index 6 or 7 and when it dismisses, the zoom transition won’t have any effect because there’s no matchedTransitionSource for that index. Therefore, indexes greater than 3 will always have a transition ID of 3
+                transitionSource = min(collection.index(newValue), 3)
+                sendUpdate(collection, id: newValue)
+            }
+            .onChange(of: isPresented) { oldValue, newValue in
+                sendUpdate(collection, id: activeTabID)
+            }
+        }
+    }
+    
+    private func sendUpdate(_ collection: SubviewsCollection, id: Subview.ID?) {
+        if let viewID = collection.first(where: { $0.id == id })?.containerValues.activeViewID {
+            updates(isPresented, viewID)
+        }
+    }
+}
+
+struct ImageViewerConfig {
+    var height: CGFloat = 150
+    var cornerRadius: CGFloat = 15
+    var spacing: CGFloat = 10
+}
+
+/// To retrieve the current active ID, we can utilize container values to pass the ID to the view and then extract it from the subview
+extension ContainerValues {
+    @Entry var activeViewID: AnyHashable?
+}
+
+#Preview {
+    ImageSliderContentView()
+}
+
+extension SubviewsCollection {
+    func index(_ id: SubviewsCollection.Element.ID?) -> Int {
+        firstIndex(where: { $0.id == id }) ?? 0
+    }
+}
