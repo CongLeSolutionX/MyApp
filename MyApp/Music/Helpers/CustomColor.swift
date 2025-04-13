730588f8d5902d07f574b34a55ddf4e3118f0b84
Cong Le <cle155@csu.fullerton.edu>
2025-04-13 03:47:55 -0700

    add Helpers section

---
+++ MyApp/Music/Helpers/CustomColor.swift
@@ -0,0 +1,20 @@
+//
+//  CustomColor.swift
+//  MyApp
+//
+//  Created by Cong Le on 4/13/25.
+//
+
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Color constants the app uses.
+*/
+
+import SwiftUI
+
+/// An extension on `Color` that provides convenience variables for commonly used, app-specific colors.
+extension Color {
+    static let musicMarathonPurple = Color(red: 0.87, green: 0.55, blue: 1.00, opacity: 1)
+}
