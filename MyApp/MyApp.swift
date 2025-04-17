418cd1efaa9be2dfb7ce386bc73c38849d355404
Cong Le <cle155@csu.fullerton.edu>
2025-04-17 06:16:35 -0700

    update code

--- MyApp/MyApp.swift
+++ MyApp/MyApp.swift
@@ -5,14 +5,56 @@
 ////  Created by Cong Le on 8/19/24.
 ////
 //
-//import SwiftUI
-//
-//// Step 3: Embed in main app structure
-//@main
-//struct MyAppApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            SpeechRecognizerView_ContentView()
-//        }
-//    }
-//}
+import SwiftUI
+import FirebaseCore
+import FirebaseAppCheck
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication
+                     .LaunchOptionsKey: Any]? = nil) -> Bool {
+    // Recommendation: Protect your Vertex AI API resources from abuse by preventing unauthorized
+    // clients using App Check; see https://firebase.google.com/docs/app-check#get_started.
+    AppCheck.setAppCheckProviderFactory(AppCheckNotConfiguredFactory())
+
+    FirebaseApp.configure()
+
+    if let firebaseApp = FirebaseApp.app(), firebaseApp.options.projectID == "mockproject-1234" {
+      guard let bundleID = Bundle.main.bundleIdentifier else { fatalError() }
+      fatalError("""
+      You must create and/or download a valid `GoogleService-Info.plist` file for \(bundleID) from \
+      https://console.firebase.google.com to run this sample. Replace the existing \
+      `GoogleService-Info.plist` file in the `FirebaseVertexAI/Sample` directory with this new file.
+      """)
+    }
+
+    return true
+  }
+}
+
+@main
+struct VertexAISampleApp: App {
+  @UIApplicationDelegateAdaptor var appDelegate: AppDelegate
+
+  var body: some Scene {
+    WindowGroup {
+        SpeechRecognizerView_ContentView()
+    }
+  }
+}
+
+/// Placeholder App Check provider factory that returns a simple ``AppCheckNotConfigured`` error.
+private class AppCheckNotConfiguredFactory: NSObject, AppCheckProviderFactory {
+  private class AppCheckNotConfiguredProvider: NSObject, AppCheckProvider {
+    func getToken() async throws -> AppCheckToken {
+      throw AppCheckNotConfigured()
+    }
+  }
+
+  func createProvider(with app: FirebaseApp) -> (any AppCheckProvider)? {
+    return AppCheckNotConfiguredProvider()
+  }
+}
+
+/// Error indicating that App Check is not configured in the sample app.
+struct AppCheckNotConfigured: Error {}
