c7e39bcf1d58476f0ac85178cd3322cf048ae8e7
Cong Le <cle155@csu.fullerton.edu>
2025-04-18 23:27:28 -0700

    update code

--- MyApp/ColorStylePreviewerApp/ColorStylePreviewerApp.swift
+++ MyApp/ColorStylePreviewerApp/ColorStylePreviewerApp.swift
@@ -1,20 +1,20 @@
+////
+////  ColorStylePreviewerApp.swift
+////  MyApp
+////
+////  Created by Cong Le on 4/18/25.
+////
 //
-//  ColorStylePreviewerApp.swift
-//  MyApp
+//// MARK: - Entry Point
+//import SwiftUI
 //
-//  Created by Cong Le on 4/18/25.
-//
-
-// MARK: - Entry Point
-import SwiftUI
-
-@main
-struct ColorStylePreviewerApp: App {
-    var body: some Scene {
-        WindowGroup {
-            NavigationStack {
-                HomeView()
-            }
-        }
-    }
-}
+//@main
+//struct ColorStylePreviewerApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            NavigationStack {
+//                HomeView()
+//            }
+//        }
+//    }
+//}
--- MyApp/MyApp.swift
+++ MyApp/MyApp.swift
@@ -1,20 +1,20 @@
-////
-////  MyApp.swift
-////  MyApp
-////
-////  Created by Cong Le on 8/19/24.
-////
 //
-//import SwiftUI
+//  MyApp.swift
+//  MyApp
 //
-//// Step 3: Embed in main app structure
-//@main
-//struct MyApp: App {
-//    var body: some Scene {
-//        WindowGroup {
-//            ContentView()
-//            //ColorPaletteDemoView()
-//            //ColorPaletteMasterView()
-//        }
-//    }
-//}
+//  Created by Cong Le on 8/19/24.
+//
+
+import SwiftUI
+
+// Step 3: Embed in main app structure
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            //ColorPaletteDemoView()
+            //ColorPaletteMasterView()
+        }
+    }
+}
